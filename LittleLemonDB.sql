59ac60ff7100bfa67aa89ef6c857deda1537e082
adiladawi <mono1983@gmail.com>
2023-02-02 10:52:24 +0700

    eer diagram and db sql file

---
+++ LittleLemonDB.sql
@@ -0,0 +1,220 @@
+-- MySQL dump 10.13  Distrib 8.0.32, for Linux (x86_64)
+--
+-- Host: 127.0.0.1    Database: LittleLemonDB
+-- ------------------------------------------------------
+-- Server version	8.0.32-0ubuntu0.22.10.2
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Bookings`
+--
+
+DROP TABLE IF EXISTS `Bookings`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Bookings` (
+  `booking_id` int NOT NULL AUTO_INCREMENT,
+  `booking_date` date DEFAULT NULL,
+  `booking_table_number` int DEFAULT NULL,
+  `customer_id` int DEFAULT NULL,
+  PRIMARY KEY (`booking_id`),
+  KEY `customer_id_fk_idx` (`customer_id`),
+  CONSTRAINT `customer_id_fk` FOREIGN KEY (`customer_id`) REFERENCES `Customer` (`customer_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Bookings`
+--
+
+LOCK TABLES `Bookings` WRITE;
+/*!40000 ALTER TABLE `Bookings` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Bookings` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Customer`
+--
+
+DROP TABLE IF EXISTS `Customer`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Customer` (
+  `customer_id` int NOT NULL AUTO_INCREMENT,
+  `customer_name` varchar(255) DEFAULT NULL,
+  `customer_address` varchar(45) DEFAULT NULL,
+  `customer_contact_no` int DEFAULT NULL,
+  `customer_email` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`customer_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Customer`
+--
+
+LOCK TABLES `Customer` WRITE;
+/*!40000 ALTER TABLE `Customer` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Customer` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Delivery`
+--
+
+DROP TABLE IF EXISTS `Delivery`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Delivery` (
+  `delivery_id` int NOT NULL AUTO_INCREMENT,
+  `delivery_date` date DEFAULT NULL,
+  `delivery_status` varchar(255) DEFAULT NULL,
+  `staff_id` int DEFAULT NULL,
+  `customer_id` int DEFAULT NULL,
+  PRIMARY KEY (`delivery_id`),
+  KEY `staff_id_fk_idx` (`staff_id`),
+  KEY `customer_id_fk_delivery_idx` (`customer_id`),
+  CONSTRAINT `customer_id_fk_delivery` FOREIGN KEY (`customer_id`) REFERENCES `Customer` (`customer_id`),
+  CONSTRAINT `staff_id_fk_delivery` FOREIGN KEY (`staff_id`) REFERENCES `Staff` (`staff_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Delivery`
+--
+
+LOCK TABLES `Delivery` WRITE;
+/*!40000 ALTER TABLE `Delivery` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Delivery` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Menu`
+--
+
+DROP TABLE IF EXISTS `Menu`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Menu` (
+  `menu_id` int NOT NULL AUTO_INCREMENT,
+  `menuitem_id` int DEFAULT NULL,
+  `menu_name` varchar(255) DEFAULT NULL,
+  `menu_cuisine` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`menu_id`),
+  KEY `menuitem_id_fk_idx` (`menuitem_id`),
+  CONSTRAINT `menuitem_id_fk` FOREIGN KEY (`menuitem_id`) REFERENCES `MenuItems` (`menuitem_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Menu`
+--
+
+LOCK TABLES `Menu` WRITE;
+/*!40000 ALTER TABLE `Menu` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Menu` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `MenuItems`
+--
+
+DROP TABLE IF EXISTS `MenuItems`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `MenuItems` (
+  `menuitem_id` int NOT NULL AUTO_INCREMENT,
+  `menuitems_course_name` varchar(255) DEFAULT NULL,
+  `menuitems_starter_name` varchar(255) DEFAULT NULL,
+  `menuitems_dessert_name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`menuitem_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `MenuItems`
+--
+
+LOCK TABLES `MenuItems` WRITE;
+/*!40000 ALTER TABLE `MenuItems` DISABLE KEYS */;
+/*!40000 ALTER TABLE `MenuItems` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Orders`
+--
+
+DROP TABLE IF EXISTS `Orders`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Orders` (
+  `order_id` int NOT NULL AUTO_INCREMENT,
+  `order_date` date DEFAULT NULL,
+  `order_quantity` int DEFAULT NULL,
+  `order_cost` decimal(10,0) DEFAULT NULL,
+  `customer_id` int DEFAULT NULL,
+  `menu_id` int DEFAULT NULL,
+  `staff_id` int DEFAULT NULL,
+  PRIMARY KEY (`order_id`),
+  KEY `customer_id_fk_idx` (`customer_id`),
+  KEY `menu_id_fk_idx` (`menu_id`),
+  KEY `staff_id_fk_idx` (`staff_id`),
+  CONSTRAINT `customer_id_fk_order` FOREIGN KEY (`customer_id`) REFERENCES `Customer` (`customer_id`),
+  CONSTRAINT `menu_id_fk` FOREIGN KEY (`menu_id`) REFERENCES `Menu` (`menu_id`),
+  CONSTRAINT `staff_id_fk` FOREIGN KEY (`staff_id`) REFERENCES `Staff` (`staff_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Orders`
+--
+
+LOCK TABLES `Orders` WRITE;
+/*!40000 ALTER TABLE `Orders` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Orders` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Staff`
+--
+
+DROP TABLE IF EXISTS `Staff`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Staff` (
+  `staff_id` int NOT NULL AUTO_INCREMENT,
+  `staff_name` varchar(255) DEFAULT NULL,
+  `staff_role` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`staff_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Staff`
+--
+
+LOCK TABLES `Staff` WRITE;
+/*!40000 ALTER TABLE `Staff` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Staff` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-02-02 10:40:49
